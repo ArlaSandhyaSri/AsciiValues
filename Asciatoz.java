708d869dc51387009dd78f5857fbb83ee17b8bd4
ArlaSandhyaSri <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-03-17 16:04:50 +0530

    Add files via upload

---
+++ Asciatoz.java
@@ -0,0 +1,9 @@
+public class Asciatoz {
+    public static void main(String args[]){
+        char ch;
+        for(ch='a';ch<='z';ch++){
+            System.out.println("Ascii values of " + ch +" is " + (int)ch );
+        }
+    }
+    
+}
---
+++ AsciiAtoZ.java
@@ -0,0 +1,9 @@
+public class AsciiAtoZ {
+    public static void main(String args[]){
+        char ch;
+        for(ch='A';ch<='Z';ch++){
+            System.out.println("Ascii value of " + ch +" is "+ (int)ch);
+        }
+    }
+    
+}
